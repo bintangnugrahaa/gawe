3545a1191ab0238c0c8c2ccefc17971c02417623
iNd0n3s1A_cyb3R_Cr1M3 <djakarta1973@gmail.com>
2024-12-31 13:25:23 +0700

    feat: Add ORM methods for User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,27 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function wallet()
+    {
+        return $this->hasOne(Wallet::class);
+    }
+
+    public function projects()
+    {
+        return $this->hasMany(Project::class, 'client_id', 'id')->orderByDesc('id');
+        // $user->projects() .... menampilkan seluruh projek dari user tersebut
+    }
+
+    public function proposals()
+    {
+        return $this->hasMany(ProjectApplicant::class, 'freelancer_id', 'id')->orderByDesc('id');
+    }
+
+    public function hasAppliedToProject($projectId)
+    {
+        return ProjectApplicant::where('project_id', $projectId)
+            ->where('freelancer_id', $this->id)
+            ->exists(); // true or false
+    }
 }
