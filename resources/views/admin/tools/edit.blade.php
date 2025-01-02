5667fbbfd5e67ebc30f0df72b0575e3780cdd40a
iNd0n3s1A_cyb3R_Cr1M3 <djakarta1973@gmail.com>
2025-01-02 20:27:23 +0700

    feat: Implement CRUD functionality for Tools data

--- resources/views/admin/tools/edit.blade.php
+++ resources/views/admin/tools/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('New Tool') }}
+            {{ __('Edit Tool') }}
         </h2>
     </x-slot>
 
@@ -17,20 +17,23 @@
                     @endforeach
                 @endif
 
-                <form method="POST" action="#" enctype="multipart/form-data">
+                <form method="POST" action="{{ route('admin.tools.update', $tool) }}" enctype="multipart/form-data">
                     @csrf
+                    @method('PUT')
 
                     <div>
                         <x-input-label for="name" :value="__('Name')" />
                         <x-text-input id="name" class="block mt-1 w-full" type="text" name="name"
-                            :value="old('name')" required autofocus autocomplete="name" />
+                            value="{{ $tool->name }}" required autofocus autocomplete="name" />
                         <x-input-error :messages="$errors->get('name')" class="mt-2" />
                     </div>
 
                     <div class="mt-4">
-                        <x-input-label for="icon" :value="__('icon')" />
-                        <x-text-input id="icon" class="block mt-1 w-full" type="file" name="icon" required
-                            autofocus autocomplete="icon" />
+                        <x-input-label for="icon" :value="__('Icon')" />
+                        <img src="{{ Storage::url($tool->icon) }}" alt=""
+                            class="rounded-2xl w-[120px] h-[90px]">
+                        <x-text-input id="icon" class="block mt-1 w-full" type="file" name="icon" autofocus
+                            autocomplete="icon" />
                         <x-input-error :messages="$errors->get('icon')" class="mt-2" />
                     </div>
 
