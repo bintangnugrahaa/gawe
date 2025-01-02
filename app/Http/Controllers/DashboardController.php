cce1e4a43fac50ba2396bd4a0ebf417618e1fffa
iNd0n3s1A_cyb3R_Cr1M3 <djakarta1973@gmail.com>
2025-01-02 22:31:01 +0700

    feat: Implement wallet transaction functionality

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\WalletTransaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    //
+    public function wallet()
+    {
+        $user = Auth::user();
+        $wallet_transactions = WalletTransaction::where('user_id', $user->id)
+            ->orderByDesc('id')
+            ->paginate(10);
+
+        return view('dashboard.wallet', compact('wallet_transactions'));
+    }
 }
