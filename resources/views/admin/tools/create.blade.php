7867f4d43b968163b92b9dd5b40999368cea5c3e
iNd0n3s1A_cyb3R_Cr1M3 <djakarta1973@gmail.com>
2025-01-05 14:53:57 +0100

    chore: Format code using Prettier for consistent styling

--- resources/views/admin/tools/create.blade.php
+++ resources/views/admin/tools/create.blade.php
@@ -20,7 +20,7 @@
                 <form method="POST" action="{{ route('admin.tools.store') }}" enctype="multipart/form-data">
                     @csrf
 
-                    <div> 
+                    <div>
                         <x-input-label for="name" :value="__('Name')" />
                         <x-text-input id="name" class="block mt-1 w-full" type="text" name="name"
                             :value="old('name')" required autofocus autocomplete="name" />
