bf677a1040d34d9c2921310783903c3a903b5748
iNd0n3s1A_cyb3R_Cr1M3 <djakarta1973@gmail.com>
2025-01-15 11:29:28 +0700

    feat: setup main application routes

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [FrontController::class, 'index'])->name('front.index');
 Route::get('/category/{category:slug}', [FrontController::class, 'category'])->name('front.category');
 Route::get('/details/{project:slug}', [FrontController::class, 'details'])->name('front.details');
+Route::get('/out-of-connect', [FrontController::class, 'out_of_connect'])->name('front.out_of_connect');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
